3e8d777605f465a6280fc10ceb173078fc52a44c
Anna Henningsen <anna@addaleax.net>
2020-04-19 22:41:06 +0200

    Cache handler function on the Worker side

--- src/worker.ts
+++ src/worker.ts
@@ -2,10 +2,30 @@ import { parentPort, MessagePort } from 'worker_threads';
 import { commonState, RequestMessage, ResponseMessage, WarmupMessage } from './common';
 commonState.isWorkerThread = true;
 
+const handlerCache : Map<string, Function> = new Map();
+
+async function getHandler (fileName : string) : Promise<Function | null> {
+  let handler = handlerCache.get(fileName);
+  if (handler !== undefined) {
+    return handler;
+  }
+
+  handler = await import(fileName);
+  if (typeof handler !== 'function') {
+    handler = (handler as any).default;
+  }
+  if (typeof handler !== 'function') {
+    return null;
+  }
+
+  handlerCache.set(fileName, handler);
+  return handler;
+}
+
 const port = parentPort as MessagePort;
 port.on('message', (message : RequestMessage | WarmupMessage) => {
   if (message.warmup) {
-    import(message.fileName).catch(throwInNextTick);
+    getHandler(message.fileName).catch(throwInNextTick);
     return;
   }
 
@@ -14,11 +34,10 @@ port.on('message', (message : RequestMessage | WarmupMessage) => {
   (async function () {
     let response : ResponseMessage;
     try {
-      let handler = await import(fileName);
-      if (typeof handler !== 'function') {
-        handler = handler.default;
+      const handler = await getHandler(fileName);
+      if (handler === null) {
+        throw new Error(`No handler functionn exported from ${fileName}`);
       }
-
       const result = await handler(task);
       response = {
         taskId,
