5f31b0376fef99f83dba339c15224f025e968cc6
Anna Henningsen <anna@addaleax.net>
2020-05-13 03:10:24 +0200

    Use C++ API in N-API example
    This is an example, so let’s use the common approach to writing
    addons in N-API :)

--- examples/n-api/binding.gyp
+++ examples/n-api/binding.gyp
@@ -1,7 +1,17 @@
 {
   "targets": [{
     "target_name": "example",
-    "include_dirs": [],
+    "include_dirs": [ "<!@(node -p \"require('node-addon-api').include\")" ],
+    'cflags!': [ '-fno-exceptions' ],
+    'cflags_cc!': [ '-fno-exceptions' ],
+    'xcode_settings': {
+      'GCC_ENABLE_CPP_EXCEPTIONS': 'YES',
+      'CLANG_CXX_LIBRARY': 'libc++',
+      'MACOSX_DEPLOYMENT_TARGET': '10.7',
+    },
+    'msvs_settings': {
+      'VCCLCompilerTool': { 'ExceptionHandling': 1 },
+    },
     "sources": [
       "./example.cc"
     ]
--- examples/n-api/example.cc
+++ examples/n-api/example.cc
@@ -1,44 +1,25 @@
-#include <node_api.h>
+#include <napi.h>
 
 #include <chrono>
 #include <thread>
 
 namespace example {
 
-#define NAPI_CALL(env, call)                                      \
-  do {                                                            \
-    napi_status status = (call);                                  \
-    if (status != napi_ok) {                                      \
-      const napi_extended_error_info* error_info = NULL;          \
-      napi_get_last_error_info((env), &error_info);               \
-      bool is_pending;                                            \
-      napi_is_exception_pending((env), &is_pending);              \
-      if (!is_pending) {                                          \
-        const char* message = (error_info->error_message == NULL) \
-            ? "empty error message"                               \
-            : error_info->error_message;                          \
-        napi_throw_error((env), NULL, message);                   \
-        return NULL;                                              \
-      }                                                           \
-    }                                                             \
-  } while(0)
+using namespace Napi;
 
 // The Run function provides the implementation of the worker.
 // info argument will contain the input arguments. To perform
 // work asynchronously, this must return a Promise.
-static napi_value Run(napi_env env, napi_callback_info info) {
+static String Run(const CallbackInfo& info) {
   // Artificially block the thread, simulating block activity
   std::this_thread::sleep_for(std::chrono::seconds(1));
-  napi_value result;
-  NAPI_CALL(env,
-    napi_create_string_latin1(env, "Hello World", NAPI_AUTO_LENGTH, &result));
-  return result;
+  return String::New(info.Env(), "Hello World");
 }
 
-NAPI_MODULE_INIT() {
-  napi_value result;
-  NAPI_CALL(env,
-    napi_create_function(env, "run", NAPI_AUTO_LENGTH, Run, NULL, &result));
-  return result;
+Object Init(Env env, Object exports) {
+  return Function::New(env, Run);
 }
+
+NODE_API_MODULE(NODE_GYP_MODULE_NAME, Init)
+
 }  // namespace example
