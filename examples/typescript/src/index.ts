5cfded95c4434d1e40683b0d1b1df3b9016a89ee
James M Snell <jasnell@gmail.com>
2020-12-02 13:52:39 -0800

    Add more examples
    * Typescript example
    * Cancelling using Node.js built in AbortController
    * Worker reporting progress back to main thread
    
    Fixes: https://github.com/piscinajs/piscina/issues/92
    Fixes: https://github.com/piscinajs/piscina/issues/91

---
+++ examples/typescript/src/index.ts
@@ -0,0 +1,20 @@
+import Piscina from '../../..';
+import { isMainThread } from 'worker_threads';
+
+interface Inputs {
+  a : number;
+  b : number;
+}
+
+if (isMainThread) {
+  const piscina = new Piscina({ filename: __filename });
+
+  (async function () {
+    const task : Inputs = { a: 1, b: 2 };
+    console.log(await piscina.runTask(task));
+  })();
+} else {
+  module.exports = ({ a, b } : Inputs) : number => {
+    return a + b;
+  };
+}
