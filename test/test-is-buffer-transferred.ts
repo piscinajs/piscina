349a7dae2d1f0315e12414baffaad77f7335a2fd
Deivu <davexdme@gmail.com>
2021-10-20 10:32:21 +0800

    feat: add tests and use concat()

--- test/test-is-buffer-transferred.ts
+++ test/test-is-buffer-transferred.ts
@@ -2,28 +2,28 @@ import Piscina from '../dist/src';
 import { test } from 'tap';
 import { resolve } from 'path';
 
-function wait() {
-  return new Promise(res => setTimeout(res, 1250));
+function wait () {
+  return new Promise((resolve) => setTimeout(resolve, 1500));
 }
 
-test('transferable objects must be transferred', async ({ not }) => {
+test('transferable objects must be transferred', async ({ equal }) => {
   const pool = new Piscina({
     filename: resolve(__dirname, 'fixtures/send-buffer-then-get-length.js'),
     useAtomics: false
   });
   await pool.run({}, { name: 'send' });
   await wait();
-  const { initial, after } = await pool.run({}, { name: 'get' });
-  not(initial, after);
+  const after = await pool.run({}, { name: 'get' });
+  equal(after, 0);
 });
 
-test('objects that implement transferable must be transferred', async ({ not }) => {
+test('objects that implement transferable must be transferred', async ({ equal }) => {
   const pool = new Piscina({
     filename: resolve(__dirname, 'fixtures/send-transferrable-then-get-length.js'),
     useAtomics: false
   });
   await pool.run({}, { name: 'send' });
   await wait();
-  const { initial, after } = await pool.run({}, { name: 'get' });
-  not(initial, after);
+  const after = await pool.run({}, { name: 'get' });
+  equal(after, 0);
 });
