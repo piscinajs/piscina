bb37c26ee5ebcb43f3daab19be790d14911a58a9
Elyahou <elyahou.i@fiverr.com>
2022-03-27 21:53:54 +0300

    lint

--- test/messages.ts
+++ test/messages.ts
@@ -4,16 +4,16 @@ import { resolve } from 'path';
 import { once } from 'events';
 
 test('Pool receive message from workers', async ({ equal }) => {
-    const pool = new Piscina({
-      filename: resolve(__dirname, 'fixtures/eval.js')
-    });
+  const pool = new Piscina({
+    filename: resolve(__dirname, 'fixtures/eval.js')
+  });
 
-    const messagePromise = once(pool, 'message');
+  const messagePromise = once(pool, 'message');
 
-    const taskResult = pool.runTask(`
+  const taskResult = pool.runTask(`
         require('worker_threads').parentPort.postMessage("some message");
         42
     `);
-    equal(await taskResult, 42);
-    equal((await messagePromise)[0], 'some message');
-  });
+  equal(await taskResult, 42);
+  equal((await messagePromise)[0], 'some message');
+});
