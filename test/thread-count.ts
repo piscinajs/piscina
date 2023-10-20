piscinajs/piscina

commit 8e6d16e1dc23f8bb39772ed954f6689852ad435f

feat: correctly handle process.exit calls outside of a task (#361)
Co-authored-by: Carlos Fuentes <me@metcoder.dev>

src/index.ts
@@ -635,33 +635,14 @@ class ThreadPool {
     });
 
     worker.on('error', (err : Error) => {
-      // Work around the bug in https://github.com/nodejs/node/pull/33394
-      worker.ref = () => {};
+      this._onError(worker, workerInfo, err, false);
+    });
 
-      // In case of an uncaught exception: Call the callback that was passed to
-      // `postTask` with the error, or emit an 'error' event if there is none.
-      const taskInfos = [...workerInfo.taskInfos.values()];
-      workerInfo.taskInfos.clear();
-
-      // Remove the worker from the list and potentially start a new Worker to
-      // replace the current one.
-      this._removeWorker(workerInfo);
-
-      if (workerInfo.isReady() && !this.workerFailsDuringBootstrap) {
-        this._ensureMinimumWorkers();
-      } else {
-        // Do not start new workers over and over if they already fail during
-        // bootstrap, there's no point.
-        this.workerFailsDuringBootstrap = true;
-      }
-
-      if (taskInfos.length > 0) {
-        for (const taskInfo of taskInfos) {
-          taskInfo.done(err, null);
-        }
-      } else {
-        this.publicInterface.emit('error', err);
-      }
+    worker.on('exit', (exitCode : number) => {
+      const err = new Error(`worker exited with code: ${exitCode}`);
+      // Only error unfinished tasks on process exit, since there are legitimate
+      // reasons to exit workers and we want to handle that gracefully when possible.
+      this._onError(worker, workerInfo, err, true);
     });
 
     worker.unref();
@@ -673,6 +654,37 @@ class ThreadPool {
     });
 
     this.workers.add(workerInfo);
+  }
+
+  _onError (worker: Worker, workerInfo: WorkerInfo, err: Error, onlyErrorUnfinishedTasks: boolean) {
+    // Work around the bug in https://github.com/nodejs/node/pull/33394
+    worker.ref = () => {};
+
+    const taskInfos = [...workerInfo.taskInfos.values()];
+    workerInfo.taskInfos.clear();
+
+    // Remove the worker from the list and potentially start a new Worker to
+    // replace the current one.
+    this._removeWorker(workerInfo);
+
+    if (workerInfo.isReady() && !this.workerFailsDuringBootstrap) {
+      this._ensureMinimumWorkers();
+    } else {
+      // Do not start new workers over and over if they already fail during
+      // bootstrap, there's no point.
+      this.workerFailsDuringBootstrap = true;
+    }
+
+    if (taskInfos.length > 0) {
+      // If there are remaining unfinished tasks, call the callback that was
+      // passed to `postTask` with the error
+      for (const taskInfo of taskInfos) {
+        taskInfo.done(err, null);
+      }
+    } else if (!onlyErrorUnfinishedTasks) {
+      // If there are no unfinished tasks, instead emit an 'error' event
+      this.publicInterface.emit('error', err);
+    }
   }
 
   _processPendingMessages () {

test/task-queue.ts
@@ -51,7 +51,7 @@ test('will put items into a task queue until they can run', async ({ equal }) =>
 
 test('will reject items over task queue limit', async ({ equal, rejects }) => {
   const pool = new Piscina({
-    filename: resolve(__dirname, 'fixtures/eval.ts'),
+    filename: resolve(__dirname, 'fixtures/eval.js'),
     minThreads: 0,
     maxThreads: 1,
     maxQueue: 2
@@ -78,7 +78,7 @@ test('will reject items over task queue limit', async ({ equal, rejects }) => {
 
 test('will reject items when task queue is unavailable', async ({ equal, rejects }) => {
   const pool = new Piscina({
-    filename: resolve(__dirname, 'fixtures/eval.ts'),
+    filename: resolve(__dirname, 'fixtures/eval.js'),
     minThreads: 0,
     maxThreads: 1,
     maxQueue: 0
@@ -97,7 +97,7 @@ test('will reject items when task queue is unavailable', async ({ equal, rejects
 
 test('will reject items when task queue is unavailable (fixed thread count)', async ({ equal, rejects }) => {
   const pool = new Piscina({
-    filename: resolve(__dirname, 'fixtures/eval.ts'),
+    filename: resolve(__dirname, 'fixtures/eval.js'),
     minThreads: 1,
     maxThreads: 1,
     maxQueue: 0

test/test-uncaught-exception-from-handler.ts
@@ -41,6 +41,46 @@ test('uncaught exception in immediate after task yields error event', async ({ e
   equal(pool.threads.length, 1);
   pool.threads[0].ref();
 
-  // This is the main aassertion here.
+  // This is the main assertion here.
   equal((await errorEvent)[0].message, 'not_caught');
+});
+
+test('exiting process resets worker', async ({ not, rejects }) => {
+  const pool = new Piscina({
+    filename: resolve(__dirname, 'fixtures/eval.js'),
+    minThreads: 1
+  });
+  const originalThreadId = pool.threads[0].threadId;
+  await rejects(pool.runTask('process.exit(1);'), /worker exited with code: 1/);
+  const newThreadId = pool.threads[0].threadId;
+  not(originalThreadId, newThreadId);
+});
+
+test('exiting process in immediate after task errors next task and resets worker', async ({ equal, not, rejects }) => {
+  const pool = new Piscina({
+    filename: resolve(__dirname, 'fixtures/eval-async.js'),
+    minThreads: 1
+  });
+
+  const originalThreadId = pool.threads[0].threadId;
+  const taskResult = await pool.runTask(`
+    setTimeout(() => { process.exit(1); }, 50);
+    42
+  `);
+  equal(taskResult, 42);
+
+  await rejects(pool.runTask(`
+  'use strict';
+
+  const { promisify } = require('util');
+  const sleep = promisify(setTimeout);
+  async function _() {
+    await sleep(1000);
+    return 42
+  }
+  _();
+  `), /worker exited with code: 1/);
+  const secondThreadId = pool.threads[0].threadId;
+
+  not(originalThreadId, secondThreadId);
 });

commit 44db1b468c932b28b1c0271a658de956cad74274

fix: do not re-create threads when calling `.destory()`
https://github.com/piscinajs/piscina/commit/8e6d16e1dc23f8bb39772ed954f6689852ad435f caused a regression that upon calling `.destroy` the threads works would be destoryed and re-created.

This is because, when calling [`worker.terminate()`](https://github.com/piscinajs/piscina/blob/f99f9810ec2fd680b9274bc43b91824fd961afee/src/index.ts#L435) the worker will exit with a status code of 1. This causes the [`_onError`](https://github.com/piscinajs/piscina/blob/f99f9810ec2fd680b9274bc43b91824fd961afee/src/index.ts#L669) method to be called which under subsequently calls  [`ensureMinimWorkers`](https://github.com/piscinajs/piscina/blob/f99f9810ec2fd680b9274bc43b91824fd961afee/src/index.ts#L695)

Closes #427

src/index.ts
@@ -545,6 +545,7 @@ class ThreadPool {
   startingUp : boolean = false;
   closingUp : boolean = false;
   workerFailsDuringBootstrap : boolean = false;
+  destroying : boolean = false;
 
   constructor (publicInterface : Piscina, options : Options) {
     this.publicInterface = publicInterface;
@@ -581,7 +582,7 @@ class ThreadPool {
   }
 
   _ensureMinimumWorkers () : void {
-    if (this.closingUp) {
+    if (this.closingUp || this.destroying) {
       return;
     }
     while (this.workers.size < this.options.minThreads) {
@@ -663,6 +664,10 @@ class ThreadPool {
     });
 
     worker.on('exit', (exitCode : number) => {
+      if (this.destroying) {
+        return;
+      }
+
       const err = new Error(`worker exited with code: ${exitCode}`);
       // Only error unfinished tasks on process exit, since there are legitimate
       // reasons to exit workers and we want to handle that gracefully when possible.
@@ -926,6 +931,7 @@ class ThreadPool {
   }
 
   async destroy () {
+    this.destroying = true;
     while (this.skipQueue.length > 0) {
       const taskInfo : TaskInfo = this.skipQueue.shift() as TaskInfo;
       taskInfo.done(new Error('Terminating worker thread'));
@@ -942,7 +948,11 @@ class ThreadPool {
       this._removeWorker(workerInfo);
     }
 
-    await Promise.all(exitEvents);
+    try {
+      await Promise.all(exitEvents);
+    } finally {
+      this.destroying = false;
+    }
   }
 
   async close (options : Required<CloseOptions>) {

test/thread-count.ts
@@ -51,3 +51,14 @@ test('conflicting min/max threads is error', async ({ throws }) => {
     maxThreads: 8
   }), /options.minThreads and options.maxThreads must not conflict/);
 });
+
+test('thread count should be 0 upon destruction', async ({ equal }) => {
+  const pool = new Piscina({
+    filename: resolve(__dirname, 'fixtures/eval.js'),
+    minThreads: 2,
+    maxThreads: 4
+  });
+  equal(pool.threads.length, 2);
+  await pool.destroy();
+  equal(pool.threads.length, 0);
+});