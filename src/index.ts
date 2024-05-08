737cb5c1496d56109cfe6766bbca14ccc962732e
JaoodxD <jaood96@gmail.com>
2024-05-08 17:06:45 +0300

    Expose current default taskQueue

--- src/index.ts
+++ src/index.ts
@@ -133,7 +133,7 @@ type EnvSpecifier = typeof Worker extends {
   new (filename : never, options?: { env: infer T }) : Worker;
 } ? T : never;
 
-class ArrayTaskQueue implements TaskQueue {
+export class ArrayTaskQueue implements TaskQueue {
   tasks : Task[] = [];
 
   get size () { return this.tasks.length; }
