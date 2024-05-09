c0707d853b0e7f20574791c9e9b16730235ca5d0
JaoodxD <jaood96@gmail.com>
2024-05-09 23:58:26 +0300

    Add test case for issue with FixedQueue Added a new test case to cover the issue with FixedQueue where removing elements from the middle of the queue leads to unexpected behavior.

--- test/fixed-queue.ts
+++ test/fixed-queue.ts
@@ -1,6 +1,6 @@
 import { test } from 'tap';
 import { Task, kQueueOptions } from '../dist/src/common';
-import FixedQueue from '../dist/src/fixed-queue';
+import { FixedQueue } from '..';
 
 class QueueTask implements Task {
   get [kQueueOptions] () {
@@ -36,4 +36,30 @@ test('queue remove', async ({ equal }) => {
   queue.remove(task);
 
   equal(queue.size, 0, 'should be empty after task removal');
+});
+
+test('removing elements from intermediate CircularBuffer should not lead to issues', async ({ equal, same }) => {
+  const queue = new FixedQueue();
+
+  const firstBatch = Array.from({ length: 2048 }, () => new QueueTask());
+  const secondBatch = Array.from({ length: 2048 }, () => new QueueTask());
+  const thirdBatch = Array.from({ length: 2048 }, () => new QueueTask());
+
+  const tasks = firstBatch.concat(secondBatch, thirdBatch);
+
+  for (const task of tasks) {
+    queue.push(task);
+  }
+  equal(queue.size, tasks.length, 'should contain 2048 * 3 items');
+
+  for (const task of secondBatch) {
+    queue.remove(task);
+  }
+
+  const expected = firstBatch.concat(thirdBatch);
+  const actual = [];
+  while (!queue.isEmpty()) {
+    actual.push(queue.shift());
+  }
+  same(actual, expected);
 });
