aee88b496823263cf7cf116e98be2ce825834ef9
Carlos Fuentes <me@metcoder.dev>
2024-08-13 16:03:52 +0200

    docs: adjust docusaurus

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://github.com/piscinajs/piscina',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/piscina/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
