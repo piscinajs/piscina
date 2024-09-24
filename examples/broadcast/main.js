692652cbd3e1aaa4f6b578d0356e83053f41a201
wanghu <18800210342@163.com>
2024-09-24 18:41:40 +0800

    add a comment about useAtomics

--- examples/broadcast/main.js
+++ examples/broadcast/main.js
@@ -6,6 +6,7 @@ const { resolve } = require('path');
 const Piscina = require('piscina');
 const piscina = new Piscina({
   filename: resolve(__dirname, 'worker.js'),
+  // Set useAtomics to false to avoid threads being blocked when idle
   useAtomics: false
 });
 
