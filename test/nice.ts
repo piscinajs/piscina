fdbc6644d372d23f2b4d2bf6d079c952f878cbd0
Carlos Fuentes <me@metcoder.dev>
2024-11-03 14:09:00 +0100

    test: scope per platform

--- test/nice.ts
+++ test/nice.ts
@@ -3,43 +3,44 @@ import { getCurrentProcessPriority, WindowsThreadPriority } from '@napi-rs/nice'
 import { resolve } from 'path';
 import { test } from 'tap';
 
-test('can set niceness for threads on Linux', {
-  skip: process.platform !== 'linux'
-}, async ({ equal }) => {
-  const worker = new Piscina({
-    filename: resolve(__dirname, 'fixtures/eval.js'),
-    niceIncrement: 5
+test('niceness - Linux:', { skip: process.platform !== 'linux' }, scope => {
+  scope.test('can set niceness for threads on Linux', async ({ equal }) => {
+    const worker = new Piscina({
+      filename: resolve(__dirname, 'fixtures/eval.js'),
+      niceIncrement: 5
+    });
+
+    // ts-ignore because the dependency is not installed on Windows.
+    // @ts-ignore
+    const currentNiceness = getCurrentProcessPriority();
+    const result = await worker.run('require("@napi-rs/nice").getCurrentProcessPriority()');
+    // niceness is capped to 19 on Linux.
+    const expected = Math.min(currentNiceness + 5, 19);
+    equal(result, expected);
   });
 
-  // ts-ignore because the dependency is not installed on Windows.
-  // @ts-ignore
-  const currentNiceness = getCurrentProcessPriority();
-  const result = await worker.run('require("@napi-rs/nice").getCurrentProcessPriority()');
+  scope.test('setting niceness never does anything bad', async ({ equal }) => {
+    const worker = new Piscina({
+      filename: resolve(__dirname, 'fixtures/eval.js'),
+      niceIncrement: 5
+    });
 
-  // niceness is capped to 19 on Linux.
-  const expected = Math.min(currentNiceness + 5, 19);
-  equal(result, expected);
+    const result = await worker.run('42');
+    equal(result, 42);
+  });
 });
 
-test('can set niceness for threads on Windows', {
+test('niceness - Windows', {
   skip: process.platform !== 'win32'
-}, async ({ equal }) => {
-  const worker = new Piscina({
-    filename: resolve(__dirname, 'fixtures/eval.js'),
-    niceIncrement: WindowsThreadPriority.ThreadPriorityAboveNormal
+}, scope => {
+  scope.test('can set niceness for threads on Windows', async ({ equal }) => {
+    const worker = new Piscina({
+      filename: resolve(__dirname, 'fixtures/eval.js'),
+      niceIncrement: WindowsThreadPriority.ThreadPriorityAboveNormal
+    });
+
+    const result = await worker.run('require("@napi-rs/nice").getCurrentProcessPriority()');
+
+    equal(result, WindowsThreadPriority.ThreadPriorityAboveNormal);
   });
-
-  const result = await worker.run('require("@napi-rs/nice").getCurrentProcessPriority()');
-
-  equal(result, WindowsThreadPriority.ThreadPriorityAboveNormal);
-});
-
-test('setting niceness never does anything bad', async ({ equal }) => {
-  const worker = new Piscina({
-    filename: resolve(__dirname, 'fixtures/eval.js'),
-    niceIncrement: 5
-  });
-
-  const result = await worker.run('42');
-  equal(result, 42);
 });
