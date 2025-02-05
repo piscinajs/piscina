4ed2ad60519c3a1df6348306e154036ac9c311e8
Carlos Fuentes <me@metcoder.dev>
2025-02-05 11:31:44 +0100

    test: add missing files

---
+++ test/fixtures/async-iterator.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-eval
+module.exports = async function* ({ length = 5, throwNext = false } = {}) {
+  const median = Math.floor(length / 2);
+  for (let i = 0; i < length; i++) {
+    if (throwNext && i === median) {
+      throw new Error('Thrown error');
+    }
+
+    yield `${i}`;
+  }
+};
