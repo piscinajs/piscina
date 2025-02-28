9627d6751b3eb57cc7aa44555118745e0cedaaef
Carlos Fuentes <me@metcoder.dev>
2025-02-28 12:12:20 +0100

    fix: linter

--- test/fixtures/async-iterator.js
+++ test/fixtures/async-iterator.js
@@ -1,17 +1,3 @@
-const { createReadStream } = require('node:fs')
-const { resolve } = require('node:path')
-
-const stream = createReadStream(
-  resolve(__dirname, '../fixtures/iterator.js')
-)
-
-// module.exports = async function* ({ length = 5, throwNext = false } = {}) {
-//   for await (const chunk of stream) {
-//     yield chunk
-//     process._rawDebug('>> async-iterator.js - chunk: ', chunk)
-//   }
-// }
-
 module.exports = async function* ({ length = 5, throwNext = false } = {}) {
   const median = Math.floor(length / 2);
   for (let i = 0; i < length; i++) {
@@ -20,6 +6,5 @@ module.exports = async function* ({ length = 5, throwNext = false } = {}) {
     }
 
     yield Buffer.from(`${i}`);
-    // yield `${i}`;
   }
 };
