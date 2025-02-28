ca203ab20fb2d81f635001dd19c0a5b2550f8c6b
Carlos Fuentes <me@metcoder.dev>
2025-02-28 12:13:59 +0100

    fix: linting

--- test/fixtures/bad-iterators.js
+++ test/fixtures/bad-iterators.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line no-eval
+ 
 module.exports = {
   asyncIterator: async function* () {
     yield Buffer.from('1');
--- test/fixtures/iterator.js
+++ test/fixtures/iterator.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line no-eval
+ 
 module.exports = function* ({ length, throwNext }) {
   const median = Math.floor(length / 2);
   for (let i = 0; i < length; i++) {
