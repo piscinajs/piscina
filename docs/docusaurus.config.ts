c8421adb47ba5c93f7387db688639c2006715c6a
Carlos Fuentes <me@metcoder.dev>
2025-05-02 12:41:35 +0200

    chore: social banner (#783)

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -57,7 +57,7 @@ const config: Config = {
   ],
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/banner.jpg',
     navbar: {
       title: 'Piscina',
       logo: {
