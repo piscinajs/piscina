d7e4afc7afa9e7adda9449b328244ba69880e397
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2025-06-24 22:02:46 +0200

    test: remove `c8`

--- scripts/run-tests.js
+++ scripts/run-tests.js
@@ -49,7 +49,7 @@ const args = [
 
 let result;
 if (isCoverage) {
-  result = spawnSync('c8', ['--reporter=lcov', 'node', ...args], { stdio: 'inherit' });
+  result = spawnSync('node', ['--experimental-test-coverage', '--test-reporter=lcov', '--test-reporter-destination=lcov.info', '--test-reporter=spec', '--test-reporter-destination=stdout' , ...args], { stdio: 'inherit' });
 } else {
   result = spawnSync('node', [...args], { stdio: 'inherit' });
 }
