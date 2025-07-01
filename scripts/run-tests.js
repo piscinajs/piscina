994761f4b202a6a5961af96b1af56a89b0171496
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2025-07-01 08:47:26 +0200

    test: remove `c8` (#823)
    (cherry picked from commit 18e86dfebae214a3fd99fd7f1e7d37a29dc2f36c)

--- scripts/run-tests.js
+++ scripts/run-tests.js
@@ -4,9 +4,9 @@
 // and considering that we could have multiple OS we manually
 // resolve the test files and pass them to the test runner
 
-const { spawnSync } = require('child_process');
-const { globSync } = require('glob');
+const { spawnSync } = require('node:child_process');
 const { parseArgs } = require('node:util');
+const { globSync } = require('glob');
 
 const options = {
   pattern: {
@@ -21,12 +21,6 @@ const options = {
     description: 'Run tests with coverage',
     default: false,
   },
-  concurrency: {
-    type: 'string',
-    short: 'n',
-    description: 'Number of test files to run concurrently',
-    default: '2',
-  },
 };
 
 const {
@@ -35,23 +29,36 @@ const {
 
 const pattern = values.pattern;
 const isCoverage = values.coverage;
-const concurrency = Number(values.concurrency);
 
 const testFiles = globSync(pattern, { absolute: true });
 
 const args = [
+  '--enable-source-maps',
   '--import=tsx',
-  `--test-concurrency=${concurrency}`,
   '--test',
   ...testFiles
 ];
 
 
 let result;
-if (isCoverage) {
-  result = spawnSync('c8', ['--reporter=lcov', 'node', ...args], { stdio: 'inherit' });
+
+// we skip coverage for node 20
+// because this issuse happen https://github.com/nodejs/node/pull/53315
+if (isCoverage && !process.version.startsWith('v20.')) {
+  result = spawnSync(
+      'node',
+      [
+        '--experimental-test-coverage',
+        '--test-reporter=lcov',
+        '--test-reporter-destination=lcov.info',
+        '--test-reporter=spec',
+        '--test-reporter-destination=stdout',
+        ...args
+      ],
+      { stdio: 'inherit' }
+  );
 } else {
-  result = spawnSync('node', [...args], { stdio: 'inherit' });
+  result = spawnSync('node', args, { stdio: 'inherit' });
 }
 
 process.exit(result.status);
