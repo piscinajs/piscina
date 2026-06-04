b7d4d614450bdaf8a833c9fc5ebe7ccd6d81f6f6
Heiner Pöpping <57671468+hpoepping@users.noreply.github.com>
2026-06-04 12:07:57 +0200

    fix: include skipQueue in queueSize calculation (#1030)
    Co-authored-by: Heiner Pöpping <heiner.poepping@tobit.com>
    Co-authored-by: Carlos Fuentes <me@metcoder.dev>

--- test/abort-task.test.ts
+++ test/abort-task.test.ts
@@ -50,10 +50,11 @@ test('tasks can be aborted through EventEmitter before running', async () => {
   const ee = new EventEmitter();
   const task1 = pool.run(bufs[0]);
   const abortable = pool.run(bufs[1], { signal: ee });
-  assert.strictEqual(pool.queueSize, 0); // Means it's running
-  assert.rejects(abortable, /The task has been aborted/);
-
+  assert.strictEqual(pool.queueSize, 1);
   ee.emit('abort');
+  assert.rejects(abortable, /The task has been aborted/);
+  assert.strictEqual(pool.queueSize, 0);
+
 
   // Wake up the thread handling the first task.
   Atomics.store(bufs[0], 0, 1);
@@ -75,9 +76,10 @@ test('abortable tasks will not share workers (abortable posted second)', async (
   const task1 = pool.run(bufs[0]);
   const ee = new EventEmitter();
   assert.rejects(pool.run(bufs[1], { signal: ee }), /The task has been aborted/);
-  assert.strictEqual(pool.queueSize, 0);
+  assert.strictEqual(pool.queueSize, 1);
 
   ee.emit('abort');
+  assert.strictEqual(pool.queueSize, 0);
 
   // Wake up the thread handling the first task.
   Atomics.store(bufs[0], 0, 1);
